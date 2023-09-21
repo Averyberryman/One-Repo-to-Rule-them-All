def68b9736f6a27110b63bad8d3f404e76a9d5ff
Avery Berryman <averyberryman@gmail.com>
2023-09-21 12:01:00 -0600

    Add initial app changes to see data from API calls

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-// Initial Commit
+import { fetchBooks, fetchMovies } from './APICalls';
 
 function App() {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  
+  useEffect(() => {
+    fetchBooks()
+      .then(data => {
+        setBooks(data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <div>
+            <p>Books:</p>
+            <ul>
+              {books.map(book => (
+                <li key={book._id}>{book.name}</li>
+              ))}
+            </ul>
+          </div>
+        )}
         <a
           className="App-link"
           href="https://reactjs.org"
