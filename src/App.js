392944dd6e4a479200656e10976dca68988fe937
Avery Berryman <averyberryman@gmail.com>
2023-09-21 12:08:08 -0600

    Add basic structure to test movie data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { fetchBooks, fetchMovies } from './APICalls';
+import { fetchBooks, fetchMovies } from './APICalls'
 
 function App() {
   const [books, setBooks] = useState([]);
-  const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
-  
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    fetchBooks()
-      .then(data => {
-        setBooks(data);
+    Promise.all([fetchBooks(), fetchMovies()])
+      .then(([booksData, moviesData]) => {
+        setBooks(booksData);
+        setMovies(moviesData);
         setLoading(false);
       })
       .catch(error => {
@@ -24,6 +25,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         {loading ? (
           <p>Loading...</p>
         ) : (
@@ -34,8 +36,24 @@ function App() {
                 <li key={book._id}>{book.name}</li>
               ))}
             </ul>
+            
+            <p>Movies:</p>
+            <div>
+              {movies.map(movie => (
+                <div key={movie._id} className="movie-card">
+                  <h3>{movie.name}</h3>
+                  <p><strong>Runtime:</strong> {movie.runtimeInMinutes} minutes</p>
+                  <p><strong>Budget:</strong> ${movie.budgetInMillions} million</p>
+                  <p><strong>Box Office Revenue:</strong> ${movie.boxOfficeRevenueInMillions} million</p>
+                  <p><strong>Academy Award Nominations:</strong> {movie.academyAwardNominations}</p>
+                  <p><strong>Academy Award Wins:</strong> {movie.academyAwardWins}</p>
+                  <p><strong>Rotten Tomatoes Score:</strong> {movie.rottenTomatoesScore}%</p>
+                </div>
+              ))}
+            </div>
           </div>
         )}
+
         <a
           className="App-link"
           href="https://reactjs.org"
