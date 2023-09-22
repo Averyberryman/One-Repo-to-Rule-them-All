b4d436f72814cfdd532d8b27d71eec84b5af5d73
Avery Berryman <averyberryman@gmail.com>
2023-09-22 15:34:12 -0600

    Change all movies to have clickable cards

--- src/Components/AllMovies/AllMovies.js
+++ src/Components/AllMovies/AllMovies.js
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom'
 function AllMovies({ movies }) {
     return (
         <div className="movies-container">
+            <h2>Movies</h2>
           {movies.map(movie => (
             <Link to={`/movie/${movie._id}`} key={movie._id}>
               <MovieCard movie={movie} />
