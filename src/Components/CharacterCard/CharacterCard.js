118ae10e1e1c6a7652714c1f5be6a438f36bed01
Avery Berryman <averyberryman@gmail.com>
2023-09-22 15:35:22 -0600

    Create clickable character card

--- src/Components/CharacterCard/CharacterCard.js
+++ src/Components/CharacterCard/CharacterCard.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import './CharacterCard.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./CharacterCard.css";
 
 function CharacterCard({ character }) {
   return (
     <div className="character-card">
-      <h3>{character.name}</h3>
-      <p><strong>Race:</strong> {character.race}</p>
-      <p><strong>Gender:</strong> {character.gender}</p>
+      <Link to={`/character/${character._id}`}>
+        <h3>{character.name}</h3>
+        <p>
+          <strong>Race:</strong> {character.race}
+        </p>
+        <p>
+          <strong>Gender:</strong> {character.gender}
+        </p>
+      </Link>
       {character.wikiUrl && (
         <p>
-          <a href={character.wikiUrl} target="_blank" rel="noopener noreferrer">Read more on LOTR Wiki</a>
+          <a href={character.wikiUrl} target="_blank" rel="noopener noreferrer">
+            Read more on LOTR Wiki
+          </a>
         </p>
       )}
     </div>
