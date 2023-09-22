d58b97b69c451e5e106a5362fbd3fbdb37a10895
Avery Berryman <averyberryman@gmail.com>
2023-09-22 12:32:23 -0600

    Add single item API calls and adjust some css

--- src/APICalls.js
+++ src/APICalls.js
@@ -46,8 +46,8 @@ const fetchMovies = () => {
       .then((data) => data.docs);
   };
   
-  const fetchQuote = () => {
-    return fetch("https://the-one-api.dev/v2/quote", {
+  const fetchSingleBook = () => {
+    return fetch("https://the-one-api.dev/v2//book/{id}", {
       method: "GET",
       headers: {
         Authorization: "Bearer K3m5T73Vg2q5K_ovcn6M",
@@ -62,8 +62,8 @@ const fetchMovies = () => {
       .then((data) => data.docs);
   };
 
-  const fetchChapter = () => {
-    return fetch("https://the-one-api.dev/v2/movie", {
+  const fetchSingleMovie = () => {
+    return fetch("https://the-one-api.dev/v2/movie/{id}", {
       method: "GET",
       headers: {
         Authorization: "Bearer K3m5T73Vg2q5K_ovcn6M",
@@ -77,8 +77,21 @@ const fetchMovies = () => {
       })
       .then((data) => data.docs);
   };
-  
-  
 
+  const fetchSingleCharacter = () => {
+    return fetch("https://the-one-api.dev/v2/character/{id}", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer K3m5T73Vg2q5K_ovcn6M",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => data.docs);
+  }
 
-export { fetchBooks, fetchMovies, fetchCharacter, fetchQuote, fetchChapter };
+export { fetchBooks, fetchMovies, fetchCharacter, fetchSingleBook, fetchSingleCharacter, fetchSingleMovie };
