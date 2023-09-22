d92731b429c47ff5657b9f57c1644126274df705
Avery Berryman <averyberryman@gmail.com>
2023-09-22 12:00:37 -0600

    Add header to App.js return

--- src/App.js
+++ src/App.js
@@ -1,49 +1,52 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { fetchBooks, fetchMovies } from './APICalls';
-import AllBooks from './Components/AllBooks/AllBooks'
+import { fetchBooks, fetchMovies, fetchCharacter } from './APICalls';
+import AllBooks from './Components/AllBooks/AllBooks';
 import AllMovies from './Components/AllMovies/AllMovies';
+import AllCharacters from './Components/AllCharacters/AllCharacters';
+import Header from './Components/Header/Header';
 
 function App() {
-  const [books, setBooks] = useState([]);
-  const [movies, setMovies] = useState([]);
-  const [loading, setLoading] = useState(true);
+    const [books, setBooks] = useState([]);
+    const [movies, setMovies] = useState([]);
+    const [characters, setCharacters] = useState([]); // don't forget to fetch characters as shown in previous steps
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const booksData = await fetchBooks();
-        const moviesData = await fetchMovies();
-        setBooks(booksData);
-        setMovies(moviesData);
-        setLoading(false);
-      } catch (error) {
-        console.error("Failed to fetch data:", error);
-        setLoading(false);
-      }
-    }
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const booksData = await fetchBooks();
+                const moviesData = await fetchMovies();
+                const charactersData = await fetchCharacter();
+                setBooks(booksData);
+                setMovies(moviesData);
+                setCharacters(charactersData);
+                setLoading(false);
+            } catch (error) {
+                console.error("Failed to fetch data:", error);
+                setLoading(false);
+            }
+        }
 
-    fetchData();
-  }, []);
+        fetchData();
+    }, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        {loading ? (
-          <p>Loading...</p>
-        ) : (
-          <>
-            <AllBooks books={books} />
-            <AllMovies movies={movies} />
-          </>
-        )}
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <main>
+                {loading ? (
+                    <p>Loading...</p>
+                ) : (
+                    <>
+                        <AllBooks books={books} />
+                        <AllMovies movies={movies} />
+                        <AllCharacters characters={characters} />
+                    </>
+                )}
+            </main>
+        </div>
+    );
 }
 
 export default App;
-
