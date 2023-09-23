950cb2039b8f5bb7e0250fe3ff55b9605e2ae1db
Avery Berryman <averyberryman@gmail.com>
2023-09-23 15:15:18 -0600

    change header for favorites navbar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header className="App-header">
-      <h1>The One Ringbase to Rule Them All</h1>
+      <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+        <h1>The One Ringbase to Rule Them All</h1>
+      </Link>
+      <nav className="navbar">
+        <Link to="/favorites">Favorites</Link>
+      </nav>
     </header>
   );
 }
